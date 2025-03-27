6a8263bca871df32f1a7f19d14939bb32024093a
efkleiven <39083168+efkleiven@users.noreply.github.com>
2025-03-27 13:53:45 +0100

    Add scripts for 2024

---
+++ script/1_Prepare_data_for_distance.R
@@ -0,0 +1,278 @@
+#******** Start Load and prepare Covariates **************************************
+# load libraries
+library(tidyverse)
+
+# make R read norwegian letters
+Sys.setlocale("LC_ALL", "no_NO.UTF-8")
+
+# load ptarmigan data
+load("./data/d_trans.rds")
+load("./data/d_obs.rds")
+
+#prepare data for distance analysis
+
+# Truncation distance
+W <- 200 # Chloe uses 200, John seems to use 600
+
+sort(unique(d_trans$LineName))
+
+## Count observations per area
+obs_count <- d_obs %>% 
+  dplyr::filter(between(DistanceToTransectLine, -0.1, W)) %>% 
+  dplyr::group_by(locality) %>%
+  dplyr::summarise(count = n())
+
+print(obs_count, n=30)
+sum(obs_count$count)
+
+#--------------------------------
+unique(d_obs$locality)
+unique(d_obs$locationID)
+
+## Set up arrays for area-specific data
+N_yearsTot <- (max(d_obs$Year) - min(d_obs$Year))+1
+N_sUnits <- length(unique(d_obs$locationID))
+N_sites <- N_years <- min_years <- max_years <- N_obs <- N_sumR_obs <- rep(NA, N_sUnits)
+
+dist <- c(); Year_obs <- c(); zeros_dist <- c()
+A <- matrix(NA, nrow=N_sUnits, ncol=N_yearsTot) 
+#y <- Year_obs <- zeros_dist <- matrix(NA, nrow = N_sUnits, ncol = max(obs_count$count))
+sumR_obs <- sumR_obs_year <- sumAd_obs<- matrix(NA, nrow = N_sUnits, ncol = max(obs_count$count))
+
+L <- N_line_year <- N_J_line_year <- N_A_line_year <- array(0, dim = c(n_distinct(d_trans$locationID), N_yearsTot))
+
+#N_a_line_year <- array(0, dim = c(N_sUnits, N_ageC, max(site_count$count), N_yearsTot))
+
+# Constants #
+#-----------#
+
+## Numbers of sites
+N_sites <- n_distinct(d_obs$locationID)
+
+## (Number of) years with data
+years_mon <- sort(unique(d_trans$Year)) - min(d_obs$Year) + 1 
+min_years <- min(years_mon)
+max_years <- max(years_mon)
+
+# Transect characteristics #
+#--------------------------#
+
+## Transect lengths
+# extract lengths from d_trans
+TransLen <- d_trans %>% 
+  dplyr::select(locationID, Year, sampleSizeValue) %>%
+  reshape2::dcast(locationID~Year, value.var = "sampleSizeValue", sum) %>%
+  arrange(locationID)
+
+# save translen for use in observation exploration
+#save(TransLen, file="data/trans_length.rds")
+
+# make seperate matrix from transect lengths
+TransLen_mat <- TransLen %>% dplyr::select(-locationID) %>% as.matrix()
+colnames(TransLen_mat) <- as.numeric(colnames(TransLen_mat)) - min(2000) + 1 # rename cols from year to numbers from 1
+
+# fill length array
+for(t in 1:N_yearsTot){
+  
+  if(t %in% years_mon){
+    L[1:N_sites, t] <- TransLen_mat[1:N_sites, which(years_mon == t)]
+  }
+}
+
+## Total covered area 
+A[1:N_sUnits, 1:N_yearsTot] <- L[,]*W*2/1000000
+
+# Observation distance from transect #
+#------------------------------------#
+temp_dist <- d_obs %>% 
+  dplyr::filter(between(DistanceToTransectLine, -0.1, W)) %>% 
+  dplyr::select(Year, DistanceToTransectLine, site) %>%
+  dplyr::mutate(Year2 = Year - (min_years) + 1)
+
+## Number of observations
+N_obs <- length(temp_dist$DistanceToTransectLine)
+#N_obs <- nrow(d_obs)
+
+## Distance to transect line
+dist[1:N_obs] <- temp_dist$DistanceToTransectLine
+
+## Observation year
+Year_obs[1:N_obs] <- temp_dist$Year2
+
+## Vector of 0's of same length as y
+zeros_dist[1:N_obs] <- rep(0, N_obs)
+
+# Number of birds/line #
+#-------------------------------------------#
+temp <- TransLen %>% dplyr::select(locationID)
+
+TaksObs <- d_obs %>% 
+  filter(between(DistanceToTransectLine, -0.1, W)) %>% # 
+  dplyr::mutate(cs = indtot) %>%
+  reshape2::dcast(locationID~Year, value.var = "cs", sum) %>%
+  dplyr::right_join(., temp, by = c("locationID" = "locationID")) %>%
+  replace(., is.na(.), 0) %>%
+  dplyr::arrange(locationID)
+
+# make maxtrix for number of birds per line
+TaksObs_mat <- TaksObs %>% dplyr::select(-locationID) %>% as.matrix() 
+colnames(TaksObs_mat) <- as.numeric(colnames(TaksObs_mat)) - min_years + 1
+
+years_mon <- as.numeric(colnames(TaksObs_mat)) # filtering observations on DistanceToTransectLine will in some cases remove all observations for a transect line within 1 year. This recalculates number of years with samples
+
+for(t in 1:N_yearsTot){
+  N_line_year[1:N_sites, t] <- TaksObs_mat[1:N_sites, t]}
+
+# converting to names from Henden model
+
+# define coefisintes for bind distance
+nD <- 8; delta=W/nD; midpt = seq(delta/2, W, delta)
+
+# define distance class
+dclass <- ifelse(temp_dist$DistanceToTransectLine<25, 1,
+                 ifelse(temp_dist$DistanceToTransectLine<50, 2,
+                        ifelse(temp_dist$DistanceToTransectLine<75, 3,
+                               ifelse(temp_dist$DistanceToTransectLine<100, 4,
+                                      ifelse(temp_dist$DistanceToTransectLine<125, 5,
+                                             ifelse(temp_dist$DistanceToTransectLine<150, 6,
+                                                    ifelse(temp_dist$DistanceToTransectLine<175, 7, 8)))))))
+# make vector with unique sites
+all_sites <- unique(d_trans$locationID)
+
+# make tibble with transect info, one row per line
+dat <- d_trans %>%
+  slice(match(all_sites, d_trans$locationID))%>%
+  dplyr::select(locality, verbatimLocality, locationID, municipality, site, clust, reg, footprintWKT)%>%
+  arrange(locationID) 
+
+dat
+
+# check number of regions and clusters
+length(unique(d_trans$locality))
+length(unique(d_trans$verbatimLocality))
+
+# check number of lines in each region
+table(dat$verbatimLocality)
+
+# check observations per region
+table(d_obs$verbatimLocality)
+
+# Filter rows where 'indtot' is less than or equal to 20
+d_obs_filtered <- d_obs %>%
+  filter(indtot <= 20)
+
+# Histogram of number of adults seen in each observation
+ggplot(d_obs_filtered, aes(x = indtot-chicks)) +
+  geom_histogram(binwidth = 1, fill = "skyblue", color = "black") +
+  labs(title = "Number of adults per observation", x = "n. adults", y = "Frequency")
+
+# Create a histogram of the remaining values in 'indtot'
+ggplot(d_obs_filtered, aes(x = indtot-chicks)) +
+  geom_histogram(binwidth = 1, fill = "skyblue", color = "black") +
+  facet_wrap(~Year)+
+  labs(title = "Number of adults per observation", x = "n. adults", y = "Frequency")
+
+#-------------------------------------------
+# checking observations with many individuals
+# observations of a lot of individuals, overrepresented in Hjemls?y and Rolvs?y in 2023 and 2024.
+d_obs[d_obs$indtot==59,]
+d_obs[d_obs$indtot==50,]
+d_obs[d_obs$indtot==46,]
+d_obs[d_obs$indtot==40,]
+d_obs[d_obs$indtot==30,]
+d_obs[d_obs$indtot==29,]
+d_obs[d_obs$indtot==28,]
+d_obs[d_obs$indtot==25,]
+d_obs[d_obs$indtot==24,]
+d_obs[d_obs$indtot==23,]
+d_obs[d_obs$indtot==22,]
+d_obs[d_obs$indtot==21,]
+d_obs[d_obs$indtot==20,]
+
+#--------------------------------------------------
+# calcualte effort (kilometers per year)
+TransLen$site <- dat$site
+TransLen$region <- dat$verbatimLocality
+TransLen$locality <- dat$locality
+
+effort <- tibble(TransLen) %>%
+  select(!c(locationID,site)) %>%
+  group_by(locality) %>%
+  summarise(across(where(is.numeric), ~sum(.x)/1000, .names = "Sum_{.col}")) %>%
+  mutate()
+
+print(effort, n=1000)
+
+# check yearly total
+colSums(effort[,-1])
+
+# write table with effort
+write.table(effort, file="data/effort_2024.txt", sep="\t", row.names=F, quote=F)
+# list of sites in each cluster
+
+# Make list of sites in different cluster
+result <- dat %>%
+  group_by(clust) %>%
+  summarise(values_in_a = list(site), .groups = "drop")
+
+print(result$values_in_a)
+
+# number of individuals seen per cluster per year
+N_line_year_2 <- as_tibble(N_line_year)
+dim(N_line_year_2)
+
+N_line_year_2 
+
+N_line_year_2$site <- dat$site
+N_line_year_2$clust <- dat$clust
+N_line_year_2$locality <- dat$locality
+
+colnames(N_line_year_2)[1:25] <- as.character(2000:2024)
+
+# number of birds seen in each cluster
+ind_cluster <- tibble(N_line_year_2) %>%
+  select(!c(site,clust)) %>%
+  group_by(locality) %>%
+  summarise(across(where(is.numeric), sum, .names = "Sum_{.col}")) %>%
+  mutate()
+
+print(ind_cluster, n=1000)
+
+# number of observations per cluster
+# Summarize count of rows for each combination of 'Year' and 'locality'
+summary_counts <- d_obs %>%
+  group_by(Year, locality) %>%
+  summarize(row_count = n(), .groups = 'drop')
+
+# Pivot the summary data to get 'Year' as columns and 'locality' as rows
+wide_summary <- summary_counts %>%
+  pivot_wider(names_from = Year, values_from = row_count, values_fill = list(row_count = 0))
+
+# View the result
+print(wide_summary)
+
+#------------------------------------------
+## Assembling all data in a list for JAGS
+input.data <- list(
+  B=W, # truncation distance
+  nD=nD, # number of distance binds
+  delta = delta, #width of distance binds
+  midpt = midpt,
+  dist = dist, # Distance to transect line for each individual observation
+  #zeros_dist = zeros_dist, # Vector of 0's of same length as y
+  nobs = N_obs, # Total number of observations
+  y = N_line_year, # Number of birds observed per site per year
+  T = N_yearsTot, # Max number of years with data
+  nsites = N_sites, # Total number of monitored sites per area
+  area = A, # Total covered area per year per site
+  dclass = dclass, # distance class for all observations
+  site = temp_dist$site, # site for each observation
+  Reg = dat$reg,
+  Clust = dat$clust,
+  NClust = length(unique(dat$clust)),
+  VerbClust = dat$locality
+)
+
+# save inputdata    
+save(input.data, file = "data/RypeData_GBIF00-24.rds")
+#- end of script
---
+++ script/2_Prepare_cov.R
@@ -0,0 +1,214 @@
+# load covariates and prepare for JAGS
+
+# load info about transect and observations
+load("data/d_obs.rds")
+load("data/d_trans.rds")
+
+# load spatial covariate (onsetS, max NDVI, OnsetF, Anomaly, rr, tg)
+all_vars <- tibble(read.csv("./data/all_vars.csv"))
+
+# manaually replacing names in d_trans to be in line with all_vars
+d_trans$locality <- replace(d_trans$locality, d_trans$locality=="Karasjok Aitevarri","Aitevarri" )
+d_trans$locality <- replace(d_trans$locality, d_trans$locality=="Karasjok Iskuras", "Iskuras" )
+d_trans$locality <- replace(d_trans$locality, d_trans$locality=="Stilla - Joatka", "Stilla" )
+d_trans$locality <- replace(d_trans$locality, d_trans$locality=="Laksfjordvidda/Kunes", "Kunes")
+d_trans$locality <- replace(d_trans$locality, d_trans$locality=="Savngovann", "Sangovann")
+d_trans$locality <- replace(d_trans$locality, d_trans$locality=="Nyborgmoen/Kroken", "Nyborgmoen")
+d_trans$locality <- replace(d_trans$locality, d_trans$locality=="Ifjord-Lebesby", "Ifjord_Lebesby")
+d_trans$locality <- replace(d_trans$locality, d_trans$locality=="Ifjord-Tana", "Ifjord_Tana")
+
+# fix norwegian letters in all_vars
+unique(all_vars$loc_coat)
+
+all_vars$loc_coat <- replace(all_vars$loc_coat, all_vars$loc_coat=="Hjelms\xf8y", "Hjelmsøy")
+all_vars$loc_coat <- replace(all_vars$loc_coat, all_vars$loc_coat=="Kj\xe6s", "Kjæs")
+all_vars$loc_coat <- replace(all_vars$loc_coat, all_vars$loc_coat=="Rolvs\xf8y", "Rolvsøy")
+all_vars$loc_coat <- replace(all_vars$loc_coat, all_vars$loc_coat=="S\xf8r\xf8ya", "Sørøya")
+
+all_sites <- unique(d_trans$locationID)
+all_reg <- unique(d_trans$verbatimLocality)
+all_clust <- unique(d_trans$locality)
+
+dat <- d_trans %>%
+  slice(match(all_sites, d_trans$locationID))%>%
+  dplyr::select(locality, verbatimLocality, locationID, municipality, site)%>%
+  arrange(locationID) %>%
+  mutate(reg = match(verbatimLocality, all_reg), 
+         clust = match(locality, all_clust) )
+
+# add column with numeric cluster
+all_vars$clust <- dat$clust[match(all_vars$loc_coat, dat$locality)]
+
+# make array with mean OnsetF for each cluster
+OnsetF <- all_vars %>%
+  group_by(year, clust) %>%
+  summarise(fall=mean(fall)) %>%
+  pivot_wider(names_from = year, values_from = fall)%>%
+  select(!clust)
+
+# make empty array
+OnsetF2 <- array(NA, dim = c(nrow(OnsetF), ncol(OnsetF)))
+
+for(i in 1:nrow(OnsetF)){
+  OnsetF2[i,] <- unlist(OnsetF[i,])
+}
+
+# make array with mean rr for each cluster
+rr <- all_vars %>%
+  group_by(year, clust) %>%
+  summarise(rr=mean(rr)) %>%
+  pivot_wider(names_from = year, values_from = rr)%>%
+  select(!clust)
+
+# make empty array
+rr2 <- array(NA, dim = c(nrow(rr), ncol(rr)))
+
+for(i in 1:nrow(rr)){
+  rr2[i,] <- unlist(rr[i,])
+}
+
+
+# make array with mean tg for each cluster
+tg <- all_vars %>%
+  group_by(year, clust) %>%
+  summarise(tg=mean(tg)) %>%
+  pivot_wider(names_from = year, values_from = tg)%>%
+  select(!clust)
+
+# make empty array
+tg2 <- array(NA, dim = c(nrow(tg), ncol(tg)))
+
+for(i in 1:nrow(tg)){
+  tg2[i,] <- unlist(tg[i,])
+}
+
+# make array with mean anom for each cluster
+anom <- all_vars %>%
+  group_by(year, clust) %>%
+  summarise(anom=mean(anom)) %>%
+  pivot_wider(names_from = year, values_from = anom)%>%
+  select(!clust)
+
+# make empty array
+anom2 <- array(NA, dim = c(nrow(anom), ncol(anom)))
+
+for(i in 1:nrow(anom)){
+  anom2[i,] <- unlist(anom[i,])
+}
+
+# replace NA with 0
+anom2[is.na(anom2)] <- 0 # should I replace with 0 or mean(anom2, na.rm=T)?
+
+# harvest 
+harv <- tibble(read.delim("./data/HarvestTotal2000-2023.txt")) %>%
+  mutate(harv = FelteWpt/Areal) %>%
+  dplyr::select(!c(Region, Jaktdager, Areal, FelteWpt))
+
+# 4 last years in Kvalsund is missing, replace with hammerfest
+print(harv, n=1000)
+
+# manually replace missing values
+harv$harv[213:216] <- harv$harv[117:120] # replaced based on location after manual check
+
+# standardize names
+harv$Kommune[harv$Kommune=="Vadso"]  <- "Vadsø"
+harv$Kommune[harv$Kommune=="Vardo"]  <- "Vardø"
+harv$Kommune[harv$Kommune=="Sorvar"] <- "Sør-Varanger"
+harv$Kommune[harv$Kommune=="Masoy"]  <- "Måsøy"
+
+# make harvest wide format
+harv2 <- harv %>%
+  pivot_wider(names_from=Aar, values_from = harv) 
+
+# make vector of positions 
+pos_hr <-  match(dat$municipality, harv2$Kommune)
+
+# remove kommune from harv
+harv3 <- harv2 %>%
+  dplyr::select(!(Kommune)) 
+
+# make empty array
+harv4 <- array(NA, dim = c(339, 24))
+
+for(i in 1:length(pos_hr)){
+  harv4[i,] <- unlist(harv3[pos_hr[i],])
+}
+
+#----------------------------------------------
+carc <- tibble(read.csv2("data/TOTV30062024215135791.csv")) %>%
+  filter(Skadetype=="Rein") %>% 
+  mutate(year = year(as_datetime(Funnetdato, format="%d.%m.%Y")),
+         month = month(as_datetime(Funnetdato, format="%d.%m.%Y")) )%>%
+  filter(month %in% c(1:6) & year %in% c(2000:2024))%>%
+  dplyr::select(year, Antall)%>%
+  group_by(year) %>%
+  summarise(carc = sum(Antall))
+
+# checking that the formatting works
+# compared to henden et al, and its similar
+plot(carc, type='b')
+
+#----------------------------
+# Rodent data
+rodOst <- tibble(read.delim("data/storskala_04-24_spring.txt")) %>% 
+  filter(season=="spring") %>%
+  mutate(rod=Mruf)%>%
+  dplyr::select(c(year, rod))%>%
+  group_by(year)%>%
+  summarise(rodO = sum(rod))
+
+# replace the missing first years with the mean for all years in region east
+rodOst2 <- tibble(year=2000:2003, rodO=rep(mean(rodOst$rodO), times=4))
+
+# bind replace missing data to the real dataset
+rodOst3 <- rbind(rodOst2, rodOst)
+
+# make year a character
+rodOst3$year <- as.character(rodOst3$year)
+
+#Inner
+rodInner <- tibble(read.table("data/karmassumS.csv", sep=",", header=T)) %>%
+  filter(seas=="SPRING")%>%
+  dplyr::select(c(year,  rufocanus))
+
+# make year a character
+rodInner$year <- as.character(rodInner$year) 
+
+# West  
+rodwest <- tibble(read.table("data/karmassumC.csv", sep=",", header=T)) %>%
+  filter(seas=="SPRING")%>%
+  dplyr::select(c(year,  rufocanus))
+
+# make year a character
+rodwest$year <- as.character(rodwest$year) 
+
+rod1 <- rodInner %>% 
+  right_join( rodwest,  by=join_by(year))%>%
+  right_join( rodOst3,  by=join_by(year))%>%
+  left_join( rodOst3,   by=join_by(year))%>%
+  dplyr::select(!year)%>% as.matrix()
+
+# scale and put in array
+rod3 <- scale(array(rod1, dim=c(25,4)))
+#-----------------------------------------
+
+W=200
+nD <- 8; delta=W/nD; midpt = seq(delta/2, W, delta)
+N_obs <- dim(d_obs)[1]
+
+#------------------------------------------
+## Assembling all data in a list for JAGS
+load("data/RypeData_GBIF00-24.rds")
+
+input.data$OnsetF = OnsetF2
+input.data$rr = rr2
+input.data$tg =tg2
+input.data$rr = rr2
+input.data$anom=anom2
+input.data$carc=carc
+input.data$rod = rod3
+input.data$harv = harv4
+
+# save inputdata    
+save(input.data, file = "./data/data_JAGS_2024.rds")
+#- End of Script
